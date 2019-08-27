2401a996f3ac50016cd9b09f585bd4c813b6110c
黄健 <13768615461@163.com>
2019-08-27 10:23:51 +0800

    修改UUID生成的字符串，去掉横线

--- src/utils/PnUtil.js
+++ src/utils/PnUtil.js
@@ -8,7 +8,7 @@ const uuid = function() {
   function S4() {
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
   }
-  return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+  return (S4() + S4() + "" + S4() + "" + S4() + "" + S4() + "" + S4() + S4() + S4());
 };
 
 /**
