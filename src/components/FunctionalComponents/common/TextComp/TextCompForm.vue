a40070f426e5e4d3f218d3f786f6c42f92ed366b
黄健 <13768615461@163.com>
2019-09-05 15:40:29 +0800

    修复UEditor编辑器部署在tomcat下ueditor.config.js文件报404的BUG

--- src/components/FunctionalComponents/common/TextComp/TextCompForm.vue
+++ src/components/FunctionalComponents/common/TextComp/TextCompForm.vue
@@ -104,7 +104,8 @@
           // 初始容器高度
           initialFrameHeight: 240,
           // 初始容器宽度
-          initialFrameWidth: '100%'
+          initialFrameWidth: '100%',
+          UEDITOR_HOME_URL: '/pagenow/static/UEditor/'
         }
       }
     },
